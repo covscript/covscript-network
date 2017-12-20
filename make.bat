1a84c67eaf0fb4e51a99ea4d9278999e89f5bf14
Michael Lee <mikecovlee@163.com>
2017-12-20 21:31:26 +0800

    修复编译脚本的错误

--- make.bat
+++ make.bat
@@ -1,4 +1,4 @@
 @mkdir build
 @cd build
 @mkdir imports
-@g++ -std=c++11 -I ..\include -shared -static -fPIC -s -O3 ..\network.cpp -lws2_32 -wsock32 -o .\imports\network.cse
+@g++ -std=c++11 -I ..\include -shared -static -fPIC -s -O3 ..\network.cpp -lws2_32 -lwsock32 -o .\imports\network.cse
