e933839277e3edb754f0e57ca2758b48965d7930
Michael Lee <mikecovlee@163.com>
2017-12-21 11:12:51 +0800

    移除测试

--- network.cpp
+++ network.cpp
@@ -1,5 +1,5 @@
 /*
-* Covariant Script Darwin Extension
+* Covariant Script Network Extension
 *
 * This program is free software: you can redistribute it and/or modify
 * it under the terms of the GNU Affero General Public License as published
@@ -27,109 +27,124 @@
 namespace network_cs_ext {
 	using namespace cs;
 	static extension network_ext;
-	static extension_t network_ext_shared=make_shared_extension(network_ext);
+	static extension_t network_ext_shared = make_shared_extension(network_ext);
 	namespace tcp {
 		static extension tcp_ext;
-		static extension_t tcp_ext_shared=make_shared_extension(tcp_ext);
+		static extension_t tcp_ext_shared = make_shared_extension(tcp_ext);
 		using socket_t=std::shared_ptr<cs_impl::network::tcp::socket>;
 		using acceptor_t=std::shared_ptr<asio::ip::tcp::acceptor>;
 		using endpoint_t=asio::ip::tcp::endpoint;
-		var acceptor(const endpoint_t& ep)
+
+		var acceptor(const endpoint_t &ep)
 		{
-			return var::make<acceptor_t>(std::make_shared<asio::ip::tcp::acceptor>(cs_impl::network::tcp::acceptor(ep)));
+			return var::make<acceptor_t>(
+			           std::make_shared<asio::ip::tcp::acceptor>(cs_impl::network::tcp::acceptor(ep)));
 		}
-		var endpoint(const string& host,number port)
+
+		var endpoint(const string &host, number port)
 		{
-			if(port<0)
+			if (port < 0)
 				throw lang_error("Port number can not under zero.");
 			else
-				return var::make<endpoint_t>(cs_impl::network::tcp::endpoint(host,static_cast<unsigned short>(port)));
+				return var::make<endpoint_t>(cs_impl::network::tcp::endpoint(host, static_cast<unsigned short>(port)));
 		}
+
 		namespace socket {
 			static extension socket_ext;
-			static extension_t socket_ext_shared=make_shared_extension(socket_ext);
+			static extension_t socket_ext_shared = make_shared_extension(socket_ext);
+
 			var socket()
 			{
 				return var::make<socket_t>(std::make_shared<cs_impl::network::tcp::socket>());
 			}
-			void connect(socket_t& sock,const endpoint_t& ep)
+
+			void connect(socket_t &sock, const endpoint_t &ep)
 			{
 				try {
 					sock->connect(ep);
 				}
-				catch(const std::exception& e) {
+				catch (const std::exception &e) {
 					throw lang_error(e.what());
 				}
 			}
-			void accept(socket_t& sock,acceptor_t& a)
+
+			void accept(socket_t &sock, acceptor_t &a)
 			{
 				try {
 					sock->accept(*a);
 				}
-				catch(const std::exception& e) {
+				catch (const std::exception &e) {
 					throw lang_error(e.what());
 				}
 			}
-			void close(socket_t& sock)
+
+			void close(socket_t &sock)
 			{
 				try {
 					sock->close();
 				}
-				catch(const std::exception& e) {
+				catch (const std::exception &e) {
 					throw lang_error(e.what());
 				}
 			}
-			bool is_open(socket_t& sock)
+
+			bool is_open(socket_t &sock)
 			{
 				return sock->is_open();
 			}
-			void receive(socket_t& sock,string& str,number max)
+
+			void receive(socket_t &sock, string &str, number max)
 			{
-				if(max<=0)
+				if (max <= 0)
 					throw lang_error("Buffer size must above zero.");
 				else {
 					try {
-						sock->receive(str,static_cast<std::size_t>(max));
+						sock->receive(str, static_cast<std::size_t>(max));
 					}
-					catch(const std::exception& e) {
+					catch (const std::exception &e) {
 						throw lang_error(e.what());
 					}
 				}
 			}
-			void send(socket_t& sock,const string& str)
+
+			void send(socket_t &sock, const string &str)
 			{
 				try {
 					sock->send(str);
 				}
-				catch(const std::exception& e) {
+				catch (const std::exception &e) {
 					throw lang_error(e.what());
 				}
 			}
-			endpoint_t remote_endpoint(socket_t& sock)
+
+			endpoint_t remote_endpoint(socket_t &sock)
 			{
 				try {
 					return sock->remote_endpoint();
 				}
-				catch(const std::exception& e) {
+				catch (const std::exception &e) {
 					throw lang_error(e.what());
 				}
 			}
 		}
 	}
+
 	void init()
 	{
 		string_cs_ext::init();
-		network_ext.add_var("tcp",var::make_protect<extension_t>(tcp::tcp_ext_shared));
-		tcp::tcp_ext.add_var("socket",var::make_constant<type>(tcp::socket::socket,typeid(tcp::socket_t).hash_code(),tcp::socket::socket_ext_shared));
-		tcp::tcp_ext.add_var("acceptor",var::make_protect<callable>(cni(tcp::acceptor),true));
-		tcp::tcp_ext.add_var("endpoint",var::make_protect<callable>(cni(tcp::endpoint),true));
-		tcp::socket::socket_ext.add_var("connect",var::make_protect<callable>(cni(tcp::socket::connect)));
-		tcp::socket::socket_ext.add_var("accept",var::make_protect<callable>(cni(tcp::socket::accept)));
-		tcp::socket::socket_ext.add_var("close",var::make_protect<callable>(cni(tcp::socket::close)));
-		tcp::socket::socket_ext.add_var("is_open",var::make_protect<callable>(cni(tcp::socket::is_open)));
-		tcp::socket::socket_ext.add_var("receive",var::make_protect<callable>(cni(tcp::socket::receive)));
-		tcp::socket::socket_ext.add_var("send",var::make_protect<callable>(cni(tcp::socket::send)));
-		tcp::socket::socket_ext.add_var("remote_endpoint",var::make_protect<callable>(cni(tcp::socket::remote_endpoint)));
+		network_ext.add_var("tcp", var::make_protect<extension_t>(tcp::tcp_ext_shared));
+		tcp::tcp_ext.add_var("socket", var::make_constant<type>(tcp::socket::socket, typeid(tcp::socket_t).hash_code(),
+		                     tcp::socket::socket_ext_shared));
+		tcp::tcp_ext.add_var("acceptor", var::make_protect<callable>(cni(tcp::acceptor), true));
+		tcp::tcp_ext.add_var("endpoint", var::make_protect<callable>(cni(tcp::endpoint), true));
+		tcp::socket::socket_ext.add_var("connect", var::make_protect<callable>(cni(tcp::socket::connect)));
+		tcp::socket::socket_ext.add_var("accept", var::make_protect<callable>(cni(tcp::socket::accept)));
+		tcp::socket::socket_ext.add_var("close", var::make_protect<callable>(cni(tcp::socket::close)));
+		tcp::socket::socket_ext.add_var("is_open", var::make_protect<callable>(cni(tcp::socket::is_open)));
+		tcp::socket::socket_ext.add_var("receive", var::make_protect<callable>(cni(tcp::socket::receive)));
+		tcp::socket::socket_ext.add_var("send", var::make_protect<callable>(cni(tcp::socket::send)));
+		tcp::socket::socket_ext.add_var("remote_endpoint",
+		                                var::make_protect<callable>(cni(tcp::socket::remote_endpoint)));
 	}
 }
 namespace cs_impl {
@@ -157,6 +172,7 @@ namespace cs_impl {
 		return "cs::network::tcp::endpoint";
 	}
 }
+
 cs::extension *cs_extension()
 {
 	network_cs_ext::init();
