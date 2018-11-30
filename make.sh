c6479ce5f0085570bf78f595f0bea18fd95647e4
Michael Lee <mikecovlee@163.com>
2018-11-30 21:06:53 +0800

    Update make.sh

--- make.sh
+++ make.sh
@@ -21,8 +21,9 @@ function set_flag() {
     fi
 }
 
-DEFAULT_CXXFLAGS="-std=c++11 -I ../include -shared -fPIC -s -O3"
-DEFAULT_LDFLAGS="-lpthread"
+
+DEFAULT_CXXFLAGS="-std=c++11 -I$CS_DEV_PATH/include -I../include -shared -fPIC -s -O3"
+DEFAULT_LDFLAGS="-L$CS_DEV_PATH/lib -lcovscript -lpthread"
 DEFAULT_CXX=g++
 
 set_flag CXX $DEFAULT_CXX
