dfb18743dd0af74b0b5c655453851c371f26cc0a
Michael Lee <mikecovlee@163.com>
2020-02-13 21:27:07 +0800

    improve timeout impl

--- include/network/network.hpp
+++ include/network/network.hpp
@@ -32,6 +32,16 @@ namespace cs_impl {
 	namespace network {
 		static asio::io_service cs_net_service;
 
+		template<typename _fT, typename ...ArgsT>
+		auto do_for(std::size_t time_ms, _fT&& func, ArgsT&&...args)
+		{
+			using ret_t = decltype(func(std::declval<ArgsT>(args)...));
+			std::future<ret_t> future = std::async(std::launch::async, func, std::forward<ArgsT>(args)...);
+			if (future.wait_for(std::chrono::milliseconds(time_ms)) != std::future_status::ready)
+				throw cs::lang_error("Connect Timeout.");
+			return future.get();
+		}
+
 		template<typename char_t=char>
 		class buffer final {
 			char_t *buff = nullptr;
@@ -91,26 +101,14 @@ namespace cs_impl {
 
 				void connect(const tcp::endpoint &ep)
 				{
-					bool timeout=false, wait=true;
-					asio::steady_timer timer(sock.get_executor(), asio::chrono::milliseconds(timeout_time));
-					sock.async_connect(ep, [&wait](const asio::error_code& error) {
-						if(error)
-							throw cs::lang_error(error.message());
-						else
-							wait=false;
-					});
-					timer.async_wait([&timeout, &wait](const asio::error_code& error) {
-						if(!error) {
-							timeout=true;
-							wait=false;
-						}
-						else
-							throw cs::lang_error(error.message());
-					});
-					do cs_net_service.run_one();
-					while(wait);
-					if(timeout)
-						throw cs::lang_error("cs::network::timeout");
+					try {
+						do_for(timeout_time, [this, &ep]{ sock.connect(ep); });
+					} catch(...) {
+						sock.close();
+						throw;
+					}
+					if (!sock.is_open())
+						throw cs::lang_error("Connect failed.");
 				}
 
 				void close()
@@ -120,26 +118,14 @@ namespace cs_impl {
 
 				void accept(tcp::acceptor &a)
 				{
-					bool timeout=false, wait=true;
-					asio::steady_timer timer(sock.get_executor(), asio::chrono::milliseconds(timeout_time));
-					a.async_accept(sock, [&wait](const asio::error_code& error) {
-						if(error)
-							throw cs::lang_error(error.message());
-						else
-							wait=false;
-					});
-					timer.async_wait([&timeout, &wait](const asio::error_code& error) {
-						if(!error) {
-							timeout=true;
-							wait=false;
-						}
-						else
-							throw cs::lang_error(error.message());
-					});
-					do cs_net_service.run_one();
-					while(wait);
-					if(timeout)
-						throw cs::lang_error("cs::network::timeout");
+					try {
+						do_for(timeout_time, [this, &a]{ a.accept(sock); });
+					} catch(...) {
+						sock.close();
+						throw;
+					}
+					if (!sock.is_open())
+						throw cs::lang_error("Accept failed.");
 				}
 
 				bool is_open()
@@ -149,14 +135,18 @@ namespace cs_impl {
 
 				std::string receive(std::size_t maximum)
 				{
-					buffer<> buff(maximum);
-					std::size_t actually = sock.read_some(asio::buffer(buff.get(), maximum));
-					return std::string(buff.get(), actually);
+					return do_for(timeout_time, [this, maximum]{
+						buffer<> buff(maximum);
+						std::size_t actually = sock.read_some(asio::buffer(buff.get(), maximum));
+						return std::string(buff.get(), actually);
+					});
 				}
 
 				void send(const std::string &s)
 				{
-					sock.write_some(asio::buffer(s));
+					do_for(timeout_time, [this, &s]{
+						sock.write_some(asio::buffer(s));
+					});
 				}
 
 				tcp::endpoint remote_endpoint()
